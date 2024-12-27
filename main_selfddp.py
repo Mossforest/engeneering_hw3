a172bb2863d26a6c126a0367905668e265c84b98
Mossforest <1596375272@qq.com>
2024-12-27 14:14:55 +0800

    try convert ddp in torch15 to torch23

--- main_selfddp.py
+++ main_selfddp.py
@@ -26,6 +26,7 @@ from torch.distributed.distributed_c10d import _get_default_group
 from torch.nn.parallel.scatter_gather import scatter_kwargs, gather
 from torch.nn.parallel.parallel_apply import parallel_apply
 from contextlib import contextmanager
+from torch.distributed.elastic.multiprocessing.errors import record
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
 parser.add_argument('data', metavar='DIR', nargs='?', default='/inspire/hdd/ws-f4d69b29-e0a5-44e6-bd92-acf4de9990f0/public-project/public/public_datas/ILSVRC/Data/CLS-LOC',
@@ -180,71 +181,52 @@ class MyDDP(nn.Module):
         #     )
         #     # TODO：(确认一下ddp有没有这个warning 感觉不对)（在等卡）
 
-            # # only create replicas for single-device CUDA modules
-            # #
-            # # TODO: we don't need to replicate params in here. they're always going to
-            # # be broadcasted using larger blocks in broadcast_coalesced, so it might be
-            # # better to not pollute the caches with these small blocks
-            # self._module_copies = replicate(self.module, self.device_ids, detach=True)
-            # self._module_copies[0] = self.module
+        #     # only create replicas for single-device CUDA modules
+        #     #
+        #     # TODO: we don't need to replicate params in here. they're always going to
+        #     # be broadcasted using larger blocks in broadcast_coalesced, so it might be
+        #     # better to not pollute the caches with these small blocks
+        #     self._module_copies = replicate(self.module, self.device_ids, detach=True)
+        #     self._module_copies[0] = self.module
 
-            # for module_copy in self._module_copies[1:]:
-            #     for param, copy_param in zip(self.module.parameters(), module_copy.parameters()):
-            #         copy_param.requires_grad = param.requires_grad
+        #     for module_copy in self._module_copies[1:]:
+        #         for param, copy_param in zip(self.module.parameters(), module_copy.parameters()):
+        #             copy_param.requires_grad = param.requires_grad
+        #     exit()
 
         # else:
         self._module_copies = [self.module]
 
         self.modules_params = [list(m.parameters()) for m in self._module_copies]
-        self.modules_buffers = [list(m.buffers()) for m in self._module_copies]
 
-        # Build tuple of (module, parameter) for all parameters that require grads.
-        modules_and_parameters = [
-            [
-                (module, parameter)
-                for module in replica.modules()
-                for parameter in filter(
-                    lambda parameter: parameter.requires_grad,
-                    module.parameters(recurse=False))
-            ] for replica in self._module_copies]
-
-        # Build list of parameters.
-        parameters = [
-            list(parameter for _, parameter in replica)
-            for replica in modules_and_parameters]
-
-        # Checks if a module will produce a sparse gradient.
-        def produces_sparse_gradient(module):
-            if isinstance(module, torch.nn.Embedding):
-                return module.sparse
-            if isinstance(module, torch.nn.EmbeddingBag):
-                return module.sparse
-            return False
-
-        # Build list of booleans indicating whether or not to expect sparse
-        # gradients for the corresponding parameters.
-        expect_sparse_gradient = [
-            list(produces_sparse_gradient(module) for module, _ in replica)
-            for replica in modules_and_parameters]
-
-        # The bucket size limit is specified in the constructor.
-        # Additionally, we allow for a single small bucket for parameters
-        # that are defined first, such that their gradients don't spill into
-        # a much larger bucket, adding unnecessary latency after gradient
-        # computation finishes. Experiments showed 1MB is a reasonable value.
-        bucket_indices = dist._compute_bucket_assignment_by_size(
-            parameters[0],
-            [1024 * 1024, self.bucket_bytes_cap],
-            expect_sparse_gradient[0])
+        parameters, expect_sparse_gradient = self._build_params_for_reducer()
+        
+        bucket_size_limits = [self.bucket_bytes_cap]
+        (
+            bucket_indices,
+            per_bucket_size_limits,
+        ) = dist._compute_bucket_assignment_by_size(
+            parameters,
+            bucket_size_limits,
+            expect_sparse_gradient,
+        )
 
         # Note: reverse list of buckets because we want to approximate the
         # order in which their gradients are produced, and assume they
         # are used in the forward pass in the order they are defined.
+        param_to_name_mapping = self._build_debug_param_to_name_mapping(parameters)
         self.reducer = dist.Reducer(
             parameters,
             list(reversed(bucket_indices)),
+            list(reversed(per_bucket_size_limits)),
             self.process_group,
-            expect_sparse_gradient)
+            expect_sparse_gradient,
+            self.bucket_bytes_cap,
+            self.find_unused_parameters,
+            False, #self.gradient_as_bucket_view,
+            param_to_name_mapping,
+            self.bucket_bytes_cap,
+        )
 
         # # passing a handle to torch.nn.SyncBatchNorm layer
         # self._passing_sync_batchnorm_handle(self._module_copies)
@@ -310,22 +292,125 @@ class MyDDP(nn.Module):
                             param.grad.zero_()
 
             # module buffer sync
-            if self.broadcast_buffers and len(self.modules_buffers[0]) > 0:
+            if self.broadcast_buffers and len(self.modules_buffers) > 0:
                 # Synchronize buffers across processes.
                 # The process with rank 0 is considered the authoritative copy.
-                dist._broadcast_coalesced(self.process_group, self.modules_buffers[0], self.broadcast_bucket_size)
+                dist._broadcast_coalesced(self.process_group, self.modules_buffers, self.broadcast_bucket_size)
                 # only do intra-node buffer sync for replicated single-device
                 # CUDA modules
                 if self.device_ids and len(self.device_ids) > 1:
                     # intra-node buffer sync
                     result = torch.cuda.comm.broadcast_coalesced(
-                        self.modules_buffers[0],
+                        self.modules_buffers,
                         self.device_ids,
                         self.broadcast_bucket_size)
                     for tensors, module_buffers in zip(result[1:],
                                                        self.modules_buffers[1:]):
                         for tensor, buffer in zip(tensors, module_buffers):
                             buffer.set_(tensor)
+    
+    def _build_debug_param_to_name_mapping(self, parameters):
+        param_to_param_index = {parameters[i]: i for i in range(len(parameters))}
+        param_set = set(parameters)
+        param_index_to_param_fqn = {}
+        for module_name, module in self.module.named_modules():
+            for param_name, param in module.named_parameters(recurse=False):
+                fqn = f"{module_name}.{param_name}"
+                # Bypass ignored parameters since those are not reduced by DDP
+                # to begin with.
+                if param.requires_grad:
+                    if param not in param_set:
+                        assert False
+                        # self._log_and_throw(
+                        #     ValueError,
+                        #     f"Param with name {fqn} found in module parameters, but not DDP parameters."
+                        #     " This indicates a bug in DDP, please report an issue to PyTorch.",
+                        # )
+                    param_index = param_to_param_index[param]
+                    param_index_to_param_fqn[param_index] = fqn
+
+        # Ensure we covered all parameters
+        if len(param_set) != len(param_index_to_param_fqn):
+            assert False
+                # ValueError,
+                # (
+                #     "Expected param to name mapping to cover all parameters, but"
+                #     f" got conflicting lengths: {len(param_set)} vs "
+                #     f"{len(param_index_to_param_fqn)}. This indicates a bug in DDP"
+                #     ", please report an issue to PyTorch."
+                # ),
+
+        return param_index_to_param_fqn
+    
+    def _build_params_for_reducer(self):
+        # Build tuple of (module, parameter) for all parameters that require grads.
+        modules_and_parameters = [
+            (module, parameter)
+            for module_name, module in self.module.named_modules()
+            for parameter in [
+                param
+                # Note that we access module.named_parameters instead of
+                # parameters(module). parameters(module) is only needed in the
+                # single-process multi device case, where it accesses replicated
+                # parameters through _former_parameters.
+                for param_name, param in module.named_parameters(recurse=False)
+                if param.requires_grad
+                # and f"{module_name}.{param_name}" not in self.parameters_to_ignore
+            ]
+        ]
+
+        # Deduplicate any parameters that might be shared across child modules.
+        memo = set()
+        modules_and_parameters = [
+            # "p not in memo" is the deduplication check.
+            # "not memo.add(p)" is always True, and it's only there to cause "add(p)" if needed.
+            (m, p)
+            for m, p in modules_and_parameters
+            if p not in memo and not memo.add(p)  # type: ignore[func-returns-value]
+        ]
+
+        # Build list of parameters.
+        parameters = [parameter for _, parameter in modules_and_parameters]
+
+        # Checks if a module will produce a sparse gradient.
+        def produces_sparse_gradient(module):
+            if isinstance(module, (torch.nn.Embedding, torch.nn.EmbeddingBag)):
+                return module.sparse
+            return False
+
+        # Build list of booleans indicating whether or not to expect sparse
+        # gradients for the corresponding parameters.
+        expect_sparse_gradient = [
+            produces_sparse_gradient(module) for module, _ in modules_and_parameters
+        ]
+
+        self._assign_modules_buffers()
+
+        return parameters, expect_sparse_gradient
+    
+    def _assign_modules_buffers(self):
+        """
+        Assign self.module.named_buffers to self.modules_buffers.
+
+        Assigns module buffers to self.modules_buffers which are then used to
+        broadcast across ranks when broadcast_buffers=True. Note that this
+        must be called every time buffers need to be synced because buffers can
+        be reassigned by user module,
+        see https://github.com/pytorch/pytorch/issues/63916.
+        """
+        # Collect buffers for modules, filtering out buffers that should be ignored.
+        named_module_buffers = [
+            (buffer, buffer_name)
+            for buffer_name, buffer in self.module.named_buffers()
+            # if buffer_name not in self.parameters_to_ignore
+        ]
+        self.modules_buffers = [
+            buffer for (buffer, buffer_name) in named_module_buffers
+        ]
+        # Dict[str, tensor] representing module buffers not ignored by DDP.
+        self.named_module_buffers = {
+            buffer_name: buffer for (buffer, buffer_name) in named_module_buffers
+        }
 
 
 def _find_tensors(obj):
@@ -340,7 +425,7 @@ def _find_tensors(obj):
         return itertools.chain(*map(_find_tensors, obj.values()))
     return []
 
-
+@record
 def main():
     args = parser.parse_args()
 
@@ -382,29 +467,29 @@ def main():
     
     # optionally resume from a checkpoint
     best_acc1 = 0
-    if args.resume:
-        if os.path.isfile(args.resume):
-            print("=> loading checkpoint '{}'".format(args.resume))
-            with open(logpath, 'a') as file:
-                file.write("=> loading checkpoint '{}'\n".format(args.resume))
-            if torch.cuda.is_available() and args.local_rank:
-                # Map model to be loaded to specified single gpu.
-                loc = 'cuda:{}'.format(args.local_rank)
-                checkpoint = torch.load(args.resume, map_location=loc)
-            else:
-                checkpoint = torch.load(args.resume)
-            args.start_epoch = checkpoint['epoch']
-            best_acc1 = checkpoint['best_acc1']
-            if args.local_rank is not None:
-                # best_acc1 may be from a checkpoint from a different GPU
-                best_acc1 = best_acc1.to(args.local_rank)
-            model.load_state_dict(checkpoint['state_dict'])
-            optimizer.load_state_dict(checkpoint['optimizer'])
-            scheduler.load_state_dict(checkpoint['scheduler'])
-            print("=> loaded checkpoint '{}' (epoch {})"
-                  .format(args.resume, checkpoint['epoch']))
-        else:
-            print("=> no checkpoint found at '{}'".format(args.resume))
+    # if args.resume:
+    #     if os.path.isfile(args.resume):
+    #         print("=> loading checkpoint '{}'".format(args.resume))
+    #         with open(logpath, 'a') as file:
+    #             file.write("=> loading checkpoint '{}'\n".format(args.resume))
+    #         if torch.cuda.is_available() and args.local_rank:
+    #             # Map model to be loaded to specified single gpu.
+    #             loc = 'cuda:{}'.format(args.local_rank)
+    #             checkpoint = torch.load(args.resume, map_location=loc)
+    #         else:
+    #             checkpoint = torch.load(args.resume)
+    #         args.start_epoch = checkpoint['epoch']
+    #         best_acc1 = checkpoint['best_acc1']
+    #         if args.local_rank is not None:
+    #             # best_acc1 may be from a checkpoint from a different GPU
+    #             best_acc1 = best_acc1.to(args.local_rank)
+    #         model.load_state_dict(checkpoint['state_dict'])
+    #         optimizer.load_state_dict(checkpoint['optimizer'])
+    #         scheduler.load_state_dict(checkpoint['scheduler'])
+    #         print("=> loaded checkpoint '{}' (epoch {})"
+    #               .format(args.resume, checkpoint['epoch']))
+    #     else:
+    #         print("=> no checkpoint found at '{}'".format(args.resume))
 
     # initial DDP
     model = MyDDP(model, device_ids=[args.local_rank], output_device=args.local_rank)
@@ -517,19 +602,19 @@ def train(train_loader, model, criterion, optimizer, epoch, device, args):
         batch_time.append(time.time() - end)
         end = time.time()
 
-        if args.local_rank == 0 and i % args.print_freq == 0:
-            print(f'round {i}({epoch}), batch_time: {batch_time[-1]:6.3f}, loss: {losses[-1]:.4e}, top1: {top1[-1]:6.2f}, top5: {top5[-1]:6.2f}')
-            with open(args.logpath, 'a') as file:
-                file.write(f'round {i}({epoch}), batch_time: {batch_time[-1]:6.3f}, loss: {losses[-1]:.4e}, top1: {top1[-1]:6.2f}, top5: {top5[-1]:6.2f}\n')
+        # if args.local_rank == 0 and i % args.print_freq == 0:
+        #     print(f'round {i}({epoch}), batch_time: {batch_time[-1]:6.3f}, loss: {losses[-1]:.4e}, top1: {top1[-1]:6.2f}, top5: {top5[-1]:6.2f}')
+        #     with open(args.logpath, 'a') as file:
+        #         file.write(f'round {i}({epoch}), batch_time: {batch_time[-1]:6.3f}, loss: {losses[-1]:.4e}, top1: {top1[-1]:6.2f}, top5: {top5[-1]:6.2f}\n')
 
     batch_time = torch.Tensor(batch_time)
     losses = torch.Tensor(losses)
     top1 = torch.Tensor(top1)
     top5 = torch.Tensor(top5)
-    if args.local_rank == 0:
-        print(f'=== epoch {epoch} average: batch_time: {batch_time.mean():6.3f}, loss: {losses.mean():.4e}, top1: {top1.mean():6.2f}, top5: {top5.mean():6.2f}')
-        with open(args.logpath, 'a') as file:
-            file.write(f'=== epoch {epoch} average: batch_time: {batch_time.mean():6.3f}, loss: {losses.mean():.4e}, top1: {top1.mean():6.2f}, top5: {top5.mean():6.2f}\n')
+    # if args.local_rank == 0:
+    #     print(f'=== epoch {epoch} average: batch_time: {batch_time.mean():6.3f}, loss: {losses.mean():.4e}, top1: {top1.mean():6.2f}, top5: {top5.mean():6.2f}')
+    #     with open(args.logpath, 'a') as file:
+    #         file.write(f'=== epoch {epoch} average: batch_time: {batch_time.mean():6.3f}, loss: {losses.mean():.4e}, top1: {top1.mean():6.2f}, top5: {top5.mean():6.2f}\n')
 
 def validate(val_loader, model, criterion, device, args):
 
@@ -558,19 +643,19 @@ def validate(val_loader, model, criterion, device, args):
             top1.append(acc1[0])
             top5.append(acc5[0])
 
-            if args.local_rank == 0 and i % args.print_freq == 0:
-                print(f'\t\tround {i}, batch_time: {batch_time[-1]:6.3f}, loss: {losses[-1]:.4e}, top1: {top1[-1]:6.2f}, top5: {top5[-1]:6.2f}')
-                with open(args.logpath, 'a') as file:
-                    file.write(f'\t\tround {i}, batch_time: {batch_time[-1]:6.3f}, loss: {losses[-1]:.4e}, top1: {top1[-1]:6.2f}, top5: {top5[-1]:6.2f}\n')
+            # if args.local_rank == 0 and i % args.print_freq == 0:
+            #     print(f'\t\tround {i}, batch_time: {batch_time[-1]:6.3f}, loss: {losses[-1]:.4e}, top1: {top1[-1]:6.2f}, top5: {top5[-1]:6.2f}')
+            #     with open(args.logpath, 'a') as file:
+            #         file.write(f'\t\tround {i}, batch_time: {batch_time[-1]:6.3f}, loss: {losses[-1]:.4e}, top1: {top1[-1]:6.2f}, top5: {top5[-1]:6.2f}\n')
 
     batch_time = torch.Tensor(batch_time)
     losses = torch.Tensor(losses)
     top1 = torch.Tensor(top1)
     top5 = torch.Tensor(top5)
-    if args.local_rank == 0:
-        print(f'final average: batch_time: {batch_time.mean():6.3f}, loss: {losses.mean():.4e}, top1: {top1.mean():6.2f}, top5: {top5.mean():6.2f}')
-        with open(args.logpath, 'a') as file:
-            file.write(f'final average: batch_time: {batch_time.mean():6.3f}, loss: {losses.mean():.4e}, top1: {top1.mean():6.2f}, top5: {top5.mean():6.2f}\n')
+    # if args.local_rank == 0:
+    #     print(f'final average: batch_time: {batch_time.mean():6.3f}, loss: {losses.mean():.4e}, top1: {top1.mean():6.2f}, top5: {top5.mean():6.2f}')
+    #     with open(args.logpath, 'a') as file:
+    #         file.write(f'final average: batch_time: {batch_time.mean():6.3f}, loss: {losses.mean():.4e}, top1: {top1.mean():6.2f}, top5: {top5.mean():6.2f}\n')
     return top1.mean()
 
 def accuracy(output, target, topk=(1,)):
@@ -591,4 +676,8 @@ def accuracy(output, target, topk=(1,)):
 
 
 if __name__ == '__main__':
+    # os.environ['MASTER_ADDR'] = 'localhost' # '127.0.0.1'
+    # os.environ['MASTER_PORT'] = '8095' # '8090'
+    os.environ['NCCL_DEBUG'] = 'INFO'
+    torch.set_printoptions(threshold=1, edgeitems=1, linewidth=100000)
     main()
